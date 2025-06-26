6b0d8d2ef6cdf89892ae0a03a3e1c344af03cc41
Nikhilesh <nikhileshgowda10@gmail.com>
2025-06-26 16:57:07 -0700

    commit1

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 3606;
 
 // Middleware
 app.use(cors());
